7a7fc0b74f2dca7eb1cd61d80ef4b02104660dcf
Onur Kayabasi <o.kayabasi@outlook.com>
2024-09-09 22:31:54 +0200

    BE Java app introduced

---
+++ be/src/main/java/net/nymtech/Main.java
@@ -0,0 +1,10 @@
+package net.nymtech;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+final class Main {
+    public static void main(String[] args) throws Exception {
+        log.info("Starting...");
+    }
+}
