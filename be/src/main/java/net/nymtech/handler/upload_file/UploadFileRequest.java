df4ecc435f2a9a2fc48211d472a11dc34b8b1c89
Onur Kayabasi <o.kayabasi@outlook.com>
2024-09-16 22:22:04 +0200

    UploadFileRequest.content added

--- be/src/main/java/net/nymtech/handler/upload_file/UploadFileRequest.java
+++ be/src/main/java/net/nymtech/handler/upload_file/UploadFileRequest.java
@@ -3,5 +3,5 @@ package net.nymtech.handler.upload_file;
 /**
  * Represents the content of {@code Request.Type.UPLOAD_FILE} typed requests.
  */
-record UploadFileRequest(String userEmail, String title, String message) {
+record UploadFileRequest(String userEmail, String title, String message, byte[] content) {
 }
