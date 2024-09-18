a6f882236426a1574abb6d20cd29f991048949c2
Onur Kayabasi <o.kayabasi@outlook.com>
2024-09-18 06:10:31 +0200

    userEmail replaced with userId

--- be/src/main/java/net/nymtech/handler/upload_file/UploadFileRequest.java
+++ be/src/main/java/net/nymtech/handler/upload_file/UploadFileRequest.java
@@ -1,7 +1,9 @@
 package net.nymtech.handler.upload_file;
 
+import java.util.UUID;
+
 /**
  * Represents the content of {@code Request.Type.UPLOAD_FILE} typed requests.
  */
-record UploadFileRequest(String userEmail, String title, String message, byte[] content) {
+record UploadFileRequest(UUID userId, String title, String message, byte[] content) {
 }
