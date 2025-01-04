bf5d0f67572d222efda0dbac2bc3cd1bc2d2047d
Onur Kayabasi <o.kayabasi@outlook.com>
2025-01-04 17:14:19 +0100

    Override props by envs

--- be/src/main/java/net/nymtech/Main.java
+++ be/src/main/java/net/nymtech/Main.java
@@ -3,7 +3,6 @@ package net.nymtech;
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
-
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -18,10 +17,22 @@ final class Main {
     try (var iStream = Main.class.getClassLoader().getResourceAsStream("application.properties")) {
       var properties = new Properties();
       properties.load(iStream);
+      overrideByEnvs(properties);
       return properties;
     } catch (IOException e) {
       log.error("application.properties couldn't be loaded!", e);
       throw e;
+    }
+  }
+
+  private static void overrideByEnvs(Properties properties) {
+    for (var prop : properties.entrySet()) {
+      String expectedEnv = prop.getKey().toString().strip().toUpperCase().replace("-", "_");
+      String envValue = System.getenv(expectedEnv);
+      if (envValue != null) {
+        properties.setProperty(prop.getKey().toString(), envValue);
+        log.debug("{} set by environment variable {}", prop.getKey().toString(), envValue);
+      }
     }
   }
 
