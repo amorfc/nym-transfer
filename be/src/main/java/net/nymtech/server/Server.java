ebe5bf575947794a781a48d04eb7269b8f1b3e8b
Onur Kayabasi <o.kayabasi@outlook.com>
2025-01-04 20:48:22 +0100

    Retry added for the server

--- be/src/main/java/net/nymtech/server/Server.java
+++ be/src/main/java/net/nymtech/server/Server.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -12,6 +13,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
 import org.glassfish.tyrus.client.ClientManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -34,10 +36,13 @@ import net.nymtech.server.response.Response;
 @Log4j2
 public final class Server {
 
+  private static final int BOOTSTRAP_TRY_COUNT = 3;
+  private static final int TIMEOUT_SECONDS = 10;
+
   private static final ExecutorService executor = Executors.newSingleThreadExecutor();
-  private static final ObjectMapper objectMapper = new ObjectMapper();
   private final CountDownLatch runningLatch = new CountDownLatch(1);
   private final CountDownLatch sessionInitiatedLatch = new CountDownLatch(1);
+  private final AtomicBoolean isRunning = new AtomicBoolean(false);
   private final Properties properties;
   private final List<BiConsumer<UUID, Response>> sentResponseConsumers;
   @Getter(AccessLevel.PACKAGE) // Testing purposes
@@ -46,10 +51,10 @@ public final class Server {
   public void run() throws InterruptedException {
     executor.submit(() -> {
       try {
-        log.info("Server is running...");
+        log.info("Server is being started...");
+        isRunning.set(true);
 
         connectToNymClient();
-        log.info("Server bootstrap completed successfully!");
 
         runningLatch.await();
         log.info("Server is stopped successfully!");
@@ -58,23 +63,41 @@ public final class Server {
       }
     });
 
-    if (!sessionInitiatedLatch.await(5, TimeUnit.SECONDS)) {
-      executor.shutdownNow();
-      throw new RuntimeException("Session couldn't be initiated in 5 seconds!");
+    log.info("Waiting for the connection...");
+    if (!sessionInitiatedLatch.await(TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+      log.info("Connection couldn't be initiated in {} seconds, it's being stopped..",
+          TIMEOUT_SECONDS);
+      stop();
     }
   }
 
   public void stop() {
-    log.info("Server is being stopped...");
-    runningLatch.countDown();
+    if (isRunning.get()) {
+      log.info("Server is being stopped...");
+      isRunning.set(false);
+      runningLatch.countDown();
+      executor.shutdown();
+    }
   }
 
-  private void connectToNymClient() throws URISyntaxException, DeploymentException, IOException {
+  private void connectToNymClient()
+      throws URISyntaxException, DeploymentException, IOException, InterruptedException {
     var endpoint = endpoint(properties);
     var config = ClientEndpointConfig.Builder.create().build();
     var nymClientUri = new URI(getPropertyOrThrow("nym-client-url"));
     var clientManager = ClientManager.createClient();
-    clientManager.connectToServer(endpoint, config, nymClientUri);
+
+    int i = 0;
+    do {
+      try {
+        Thread.sleep(Duration.ofSeconds(2 * i));
+        log.info("Attempting for the connection for the {}. times...", ++i);
+        clientManager.connectToServer(endpoint, config, nymClientUri);
+        break;
+      } catch (Exception e) {
+        log.error("Connection attempt failed for the {}. times!", i, e);
+      }
+    } while (i < BOOTSTRAP_TRY_COUNT);
   }
 
   private Endpoint endpoint(Properties properties) {
@@ -85,12 +108,13 @@ public final class Server {
         nymClient = NymClient.build(session, sentResponseConsumers);
         session.addMessageHandler(messageHandler());
         sessionInitiatedLatch.countDown();
-        log.info("Connection with NYM Client is established successfully!");
+        log.info("Connection is successfully initiated!");
       }
     };
   }
 
   private MessageHandler.Whole<ByteBuffer> messageHandler() {
+    var objectMapper = new ObjectMapper();
     var basePath = getPropertyOrThrow("base-path");
     var uploadFileHandler = new UploadFileHandler(objectMapper, basePath);
     var downloadFileHandler = new DownloadFileHandler(objectMapper, basePath);
