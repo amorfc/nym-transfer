6ba854b1e5d5932cd3540628562d3dfbcf46fe23
amorf <fatihermetin@gmail.com>
2025-01-09 23:30:31 -0500

    Refactor AppLayout component to enhance connection management and user notifications
    - Introduced a delay before invoking the keepAliveNymClient mutation to improve connection stability.
    - Added a dispatch for setting the connection state to true upon component mount.
    - Updated the Download button to trigger an error notification when clicked.
    - Wrapped the Outlet component in a TransitionWrapper for improved animation during route changes.
    - Disabled wave effect in ConfigProvider for a more consistent UI experience.

--- fe/src/components/layout/AppLayout.tsx
+++ fe/src/components/layout/AppLayout.tsx
@@ -1,7 +1,6 @@
 import NymLayout from "@/components/common/NymLayout";
 import { useTheme } from "@/theme/themeConfig";
 import NymControlCenter from "@/components/common/NymControlCenter";
-import "./AppLayout.css";
 import { ConfigProvider, Layout } from "antd";
 import { Outlet, useLocation } from "react-router";
 import NymCard from "@/components/common/NymCard";
@@ -10,11 +9,17 @@ import NymButton from "@/components/common/NymButton";
 import { useEffect } from "react";
 import { useKeepAliveNymClientMutation } from "@/store/api/nymApi";
 import { useAppNavigation } from "@/hooks/navigation/useAppNavigation";
+import TransitionWrapper from "@/components/animation/TransitionWrapper";
+import { notifyError } from "@/utils/GlobalNotification";
+import { setIsConnecting } from "@/store/slice/nymClientSlice";
+import { useAppDispatch } from "@/hooks/useAppStore";
+
 const { Content } = Layout;
 
 function AppLayout() {
   const theme = useTheme();
   const { goToUpload, goToDownload } = useAppNavigation();
+  const dispatch = useAppDispatch();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [keepAliveNymClient, _data] = useKeepAliveNymClientMutation();
 
@@ -27,12 +32,17 @@ function AppLayout() {
   }, [location, goToUpload]);
 
   useEffect(() => {
+    dispatch(setIsConnecting(true));
     // Start the connection management when the app loads
+    const timeout = setTimeout(() => {
     keepAliveNymClient();
-  }, [keepAliveNymClient]);
+    }, 2000);
+
+    return () => clearInterval(timeout);
+  }, [dispatch, keepAliveNymClient]);
 
   return (
-    <ConfigProvider wave={{ disabled: false }} theme={theme}>
+    <ConfigProvider wave={{ disabled: true }} theme={theme}>
       <NymLayout>
         <NymButton onClick={() => goToUpload()}>Upload</NymButton>
         <NymButton onClick={() => goToDownload("asdfasdfadsf")}>
@@ -42,13 +52,15 @@ function AppLayout() {
           style={{
             display: "flex",
             alignItems: "center",
-            justifyContent: "flex-end",
+            justifyContent: "center",
           }}
         >
           <NymCard>
+            <TransitionWrapper>
             <NymFlexContainer vertical gap={12}>
               <Outlet />
             </NymFlexContainer>
+            </TransitionWrapper>
           </NymCard>
 
           <NymControlCenter />
